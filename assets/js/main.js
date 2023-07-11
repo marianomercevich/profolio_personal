a74cfc9a183a52d801fb5997f93a5be6a72a13a7
Mariano Mercevich <marianomercevich@gmail.com>
2023-07-10 21:32:22 -0300

    correccion de txt, animacion typed

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,6 +101,21 @@
       scrollto(this.hash)
     }
   }, true)
+  /**
+   * Hero type effect
+   */
+  const typed = select('.typed') 
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
 
   /**
    * Activate/show sections on load with hash links
